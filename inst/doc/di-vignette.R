6fe2dfbda5f2876a69afb632fb489089a348b907
Ilya Y. Zhbannikov <ilya.zhbannikov@duke.edu>
2018-04-11 04:24:20 +0000

    version 1.1.4

--- inst/doc/di-vignette.R
+++ inst/doc/di-vignette.R
@@ -2,11 +2,12 @@
 
 library(di)
 # Simulate database of N individuals:
-N <- 100
+N <- 1000
 dd <- data.frame(subj=seq(1:N), 
                  var1=rbinom(N,1,.5), 
                  var2=rbinom(N,1,.5), 
-                 var3=rbinom(N,1,.5))
+                 var3=rbinom(N,1,.5),
+                 age=rnorm(N, 80, 20))
 # Calculate DI
 ddi <- di(dd, c("var1", "var2", "var3"))
 
@@ -21,4 +22,7 @@ ddi <- di(dd, c("var1", "var2", "var3"), rescale.avoid = c("var2"))
 
 ## ------------------------------------------------------------------------
 ddi <- di(dd, c("var1", "var2", "var3"), rescale = FALSE, invert=c("var1", "var3"))
+
+## ------------------------------------------------------------------------
+ddi <- di(dd, c("var1", "var2", "var3"), age="age", visible=TRUE)
 
